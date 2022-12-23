baeb6ad3a03d261cab11f06c662c5680df6e4011
Jose Javier Merchante <jjmerchante@bitergia.com>
2022-12-23 08:12:38 +0100

    [SortingHat] Include generate_uuid method in the client.
    This commit includes the generate_uuid method in the cli module
    to be importable from external tools.
    
    Signed-off-by: Jose Javier Merchante <jjmerchante@bitergia.com>

--- sortinghat/cli/utils.py
+++ sortinghat/cli/utils.py
@@ -21,8 +21,10 @@
 
 import contextlib
 import functools
+import hashlib
 
 import os.path
+import unicodedata
 
 import click
 import jinja2
@@ -154,3 +156,83 @@ def display(template, nl=True, **kwargs):
     t = env.get_template(template)
     s = t.render(**kwargs)
     click.echo(s, nl=nl)
+
+
+def unaccent_string(unistr):
+    """Convert a Unicode string to its canonical form without accents.
+
+    This allows to convert Unicode strings which include accent
+    characters to their unaccent canonical form. For instance,
+    characters 'Ê, ê, é, ë' are considered the same character as 'e';
+    characters 'Ĉ, ć' are the same as 'c'.
+
+    :param unistr: Unicode string to unaccent
+
+    :returns: Unicode string on its canonical form
+    """
+    if not isinstance(unistr, str):
+        msg = "argument must be a string; {} given".format(unistr.__class__.__name__)
+        raise TypeError(msg)
+
+    cs = [c for c in unicodedata.normalize('NFD', unistr)
+          if unicodedata.category(c) != 'Mn']
+    string = ''.join(cs)
+
+    return string
+
+
+def generate_uuid(source, email=None, name=None, username=None):
+    """Generate a UUID related to identity data.
+
+    Based on the input data, the function will return the UUID
+    associated to an identity. On this version, the UUID will
+    be the SHA1 of `source:email:name:username` string.
+
+    This string is case insensitive, which means same values
+    for the input parameters in upper or lower case will produce
+    the same UUID.
+
+    The value of `name` will converted to its unaccent form which
+    means same values with accent or unaccent chars (i.e 'ö and o')
+    will generate the same UUID.
+
+    For instance, these combinations will produce the same UUID:
+        ('scm', 'jsmith@example.com', 'John Smith', 'jsmith'),
+        ('scm', 'jsmith@example,com', 'Jöhn Smith', 'jsmith'),
+        ('scm', 'jsmith@example.com', 'John Smith', 'JSMITH'),
+        ('scm', 'jsmith@example.com', 'john Smith', 'jsmith')
+
+    :param source: data source
+    :param email: email of the identity
+    :param name: full name of the identity
+    :param username: user name used by the identity
+
+    :returns: a universal unique identifier for Sorting Hat
+
+    :raises ValueError: when source is `None` or empty; each one
+        of the parameters is `None`; or the parameters are empty.
+    """
+    def to_str(value, unaccent=False):
+        s = str(value)
+        if unaccent:
+            return unaccent_string(s)
+        else:
+            return s
+
+    if source is None:
+        raise ValueError("'source' cannot be None")
+    if source == '':
+        raise ValueError("'source' cannot be an empty string")
+    if not (email or name or username):
+        raise ValueError("identity data cannot be empty")
+
+    s = ':'.join((to_str(source),
+                  to_str(email),
+                  to_str(name, unaccent=True),
+                  to_str(username))).lower()
+    s = s.encode('UTF-8', errors="surrogateescape")
+
+    sha1 = hashlib.sha1(s)
+    uuid = sha1.hexdigest()
+
+    return uuid
